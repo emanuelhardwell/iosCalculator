cb1f4ad41d1afd13e812a64b7a0464572b863b97
emanuelhardwell <emanuelnoe16@gmail.com>
2024-06-12 13:10:44 -0600

    feat: The project was cleaned

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export const App = () => {
+  return (
+    <View>
+      <Text>Hola </Text>
+    </View>
+  );
+};
